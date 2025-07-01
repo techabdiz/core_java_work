77cc6fb6f75475de571eadd531c9f18c5d0f857f
Aziz Shaikh <tech.abdiz@gmail.com>
2025-07-01 23:16:04 +0530

    added gitignore

--- src/main/java/com/deadspider/FluxSampler.java
+++ src/main/java/com/deadspider/FluxSampler.java
@@ -9,11 +9,22 @@ public class FluxSampler {
         MonoSampler.getUriWords("https://www.ietf.org/rfc/rfc2616.txt")
             .flatMapMany(Flux::fromIterable)
             .mapNotNull(String::trim)
+            .filter(s -> s.matches("[a-zA-Z0-9]+"))
+            .filter(s -> s.length() > 1)
             .filter(s -> !s.equals(System.lineSeparator()))
             .filter(s -> !s.isBlank())
-            .subscribe(System.out::println);
-            
+            .subscribe(System.out::println, 
+                error -> System.err.println("Error: " + error.getMessage()),
+                () -> System.out.println("Completed processing words."));
 
     }
 
+    public static String toUnicode(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            sb.append(String.format("\\u%04x", (int) c));
+        }
+        return sb.toString();
+    }
+    
 }
