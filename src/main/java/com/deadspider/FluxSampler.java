58d3f31ab2b9040af7d1743c7de8c75ff7d458ee
Aziz Shaikh <tech.abdiz@gmail.com>
2025-07-01 23:03:47 +0530

    flux work

--- src/main/java/com/deadspider/FluxSampler.java
+++ src/main/java/com/deadspider/FluxSampler.java
@@ -2,21 +2,18 @@ package com.deadspider;
 
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public class FluxSampler {
 
     public static void main(String[] args) {
-       /* MonoSampler
-            .getUriWords("https://www.ietf.org/rfc/rfc2616.txt")
-            .subscribe(words -> { 
-                Flux.fromIterable(words)
-                    .subscribe(System.out::println);
-            });
-            */
+        MonoSampler.getUriWords("https://www.ietf.org/rfc/rfc2616.txt")
+            .flatMapMany(Flux::fromIterable)
+            .mapNotNull(String::trim)
+            .filter(s -> !s.equals(System.lineSeparator()))
+            .filter(s -> !s.isBlank())
+            .subscribe(System.out::println);
+            
 
-            Flux.from((s)->Mono.just("initial value: " + s))
-                .subscribe(System.out::println);
     }
 
 }
